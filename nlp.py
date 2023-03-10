b5a80061f4225f1dcd5c3c8dbbedf43165f89b1b
Paul Schofield <paul.schofield.us@gmail.com>
2023-03-10 11:16:25 -0500

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -69,3 +69,38 @@ print(word_index) ## {'<OOV>': 1, 'today': 2, 'is': 3, 'a': 4, 'sunny': 5, 'day'
 ## pre OOV below prints [[1, 2, 3, 5], [7, 6]] which loses context
 print(test_sequences)
 ## currently prints [[2, 3, 4, 1, 6], [1, 8, 1, 7, 1]] which is closer to the original meaning
+
+
+## padding
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+sentences = [
+    'Today is a sunny day',
+    'Today is a rainy day',
+    'Is it sunny today?',
+    'I really enjoy walking in the snow today'
+]
+
+test_data = [
+    'Today is a snowy day',
+    'Will it be rainy tomorrow?'
+]
+
+tokenizer = Tokenizer(num_words = 100, oov_token="<OOV>")
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+
+## defaults to prepadding; maxlen, truncating optional
+sequences = tokenizer.texts_to_sequences(sentences)
+padded = pad_sequences(sequences, padding="post", maxlen=6, truncating="post")
+
+test_sequences = tokenizer.texts_to_sequences(test_data)
+print(word_index)
+print(padded)
+## [[ 2  3  4  5  6  0]
+## [ 2  3  4  7  6  0]
+## [ 3  8  5  2  0  0]
+## [ 9 10 11 12 13 14]]
